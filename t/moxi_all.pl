b1e345af390e394656a26a88312b6204ba495d08
Steve Yen <steve.yen@gmail.com>
2011-05-06 18:19:04 -0700

    Fix auth B2B tests that depended on old timeout config
    A previous commit (0b23b54d4662) changed the default timeout settings
    to be more user friendly for client-side moxi cases, but some auth B2B
    tests were depending on the old (rather generous) timeout settings,
    which this change now makes explicit.
    
    Change-Id: Id23365ade273ec301e5c2e7ecc1b068e40f6e521
    Reviewed-on: http://review.membase.org/6037
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- t/moxi_all.pl
+++ t/moxi_all.pl
@@ -49,6 +49,10 @@ print "------------------------------------ auth\n";
 my $cmd = "./t/moxi_mock.pl moxi_mock_auth binary \"\"" .
                  " url=http://127.0.0.1:4567/pools/default/buckets/default" .
                  " usr=TheUser,pwd=ThePassword,port_listen=11333," .
+                   "downstream_timeout=0," .
+                   "downstream_conn_queue_timeout=0,wait_queue_timeout=0," .
+                   "connect_timeout=5000,auth_timeout=0," .
+                   "connect_max_errors=0,connect_retry_interval=0," .
                  " ./t/rest_mock.rb";
 print($cmd . "\n");
 my $res = system($cmd);
@@ -64,6 +68,10 @@ print "------------------------------------ multitenancy\n";
 my $cmd = "./t/moxi_mock.pl moxi_multitenancy binary \"\"" .
                  " url=http://127.0.0.1:4567/pools/default/buckets/default" .
                  " default_bucket_name=default,port_listen=11333," .
+                   "downstream_timeout=0," .
+                   "downstream_conn_queue_timeout=0,wait_queue_timeout=0," .
+                   "connect_timeout=5000,auth_timeout=0," .
+                   "connect_max_errors=0,connect_retry_interval=0," .
                  " \"./t/rest_mock.rb ./t/moxi_multitenancy_rest.cfg\"";
 print($cmd . "\n");
 my $res = system($cmd);
@@ -79,6 +87,10 @@ print "------------------------------------ multitenancy_default\n";
 my $cmd = "./t/moxi_mock.pl moxi_multitenancy_default binary \"\"" .
                  " url=http://127.0.0.1:4567/pools/default/buckets/default" .
                  " default_bucket_name=default,port_listen=11333," .
+                   "downstream_timeout=0," .
+                   "downstream_conn_queue_timeout=0,wait_queue_timeout=0," .
+                   "connect_timeout=5000,auth_timeout=0," .
+                   "connect_max_errors=0,connect_retry_interval=0," .
                  " \"./t/rest_mock.rb ./t/moxi_multitenancy_rest_default.cfg\"";
 print($cmd . "\n");
 my $res = system($cmd);
